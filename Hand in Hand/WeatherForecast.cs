210c54d84bd70e43c0800f8f61702ae272c343db
Patti Perry <pattiperry12@gmail.com>
2021-06-22 13:00:45 -0500

    initial commit

---
+++ Hand in Hand/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Hand_in_Hand
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
