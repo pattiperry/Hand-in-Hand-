845349b377d0e496a005decee9cd929eceb8b71b
Patti Perry <pattiperry12@gmail.com>
2021-07-08 23:01:47 -0400

    organization.js fixed with icons

--- Hand in Hand/client/src/components/organization/Organization.js
+++ Hand in Hand/client/src/components/organization/Organization.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+import { CardLink } from "reactstrap";
 import  Card  from 'react-bootstrap/Card';
+import { MdPerson } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+import { MdPhone } from "react-icons/md";
+import { MdDomain } from "react-icons/md";
+import { MdLaptop } from "react-icons/md";
 import "./Organization.css";
 
 export const Organization = ({ organization }) => {
@@ -11,14 +16,16 @@ export const Organization = ({ organization }) => {
     <Card.Header><Card.Title>{organization.organizationName}</Card.Title></Card.Header>
 
     <Card.Body>
+    <MdDomain />{organization.location}<p></p>
+    <MdPhone />{organization.phone}<p></p>
+    
+    <MdLaptop /><CardLink href="#">{organization.url}</CardLink>
+        <p></p>
       
-    <Card.Text>
-      <p>{organization.url}</p>
-      <p>{organization.location}</p>
-      <p>{organization.phone}</p>
-      <p>{organization.email}</p>
-      <p>{organization.contactPerson}</p>
-    </Card.Text>
+    <MdPerson />{organization.contactPerson}<p></p>
+    <MdEmail />{organization.email}<p></p>
+      
+    
 
     </Card.Body>
   </Card>
